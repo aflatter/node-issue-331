13f3e7978d7c381fa8b1d27fbccc9170eb0161e4
Alexander Flatter <flatter@gmail.com>
2010-11-01 02:29:48 +0100

    Some more assertions

--- jsdom/test3.js
+++ jsdom/test3.js
@@ -5,19 +5,20 @@ var Script = process.binding("evals").Script;
 var sandbox = {x: "x"};
 sandbox.window  = sandbox;
 
-var code = "function y() { return this.x } " + 
-           "function z() { return window.x } " +
-           "function w() { return window } " +
-           "var box = {contents: window}; " +
-           "var q = y()";
+var code = "function y()  { return this.x } "         + 
+           "function z()  { return window.x } "       +
+           "function z1() { return x } "              +
+           "function w()  { return window } "         +
+           "function w1() { return window.window } "  +
+           "var box = {contents: window}; "           +
+           "var q = y();"                             +
+           "var r = z();";
 
 var result = Script.runInNewContext(code, sandbox);
 
-// Just for fun:
-// TypeError: Cannot convert object to primitive value
-common.debug(sandbox.w());
-// TypeError: Converting circular structure to JSON
-common.inspect(sandbox.w());
+// Passes 
+common.debug("sandbox.r == 'x'");
+assert.equal(sandbox.r, "x");
 
 // Passes 
 common.debug("sandbox.q == 'x'");
@@ -27,15 +28,36 @@ assert.equal(sandbox.q, "x");
 common.debug("sandbox.w() == sandbox.box.contents");
 assert.equal(sandbox.w(), sandbox.box.contents);
 
-// Fails
-// TypeError: Converting circular structure to JSON
+// Passes (though not intuitive)
 common.debug("sandbox.w() == sandbox");
-assert.equal(sandbox.w(), sandbox);
+assert.notEqual(sandbox.w(), sandbox);
+
+// Passes (though not intuitive)
+common.debug("sandbox.w() == sandbox.window");
+assert.notEqual(sandbox.w(), sandbox.window);
+
+// common.debug(sandbox.w());
+// => TypeError: Cannot convert object to primitive value
+// common.inspect(sandbox.w());
+// => TypeError: Converting circular structure to JSON
+
+// INTERESTING: Passes
+// common.debug(sandbox.w1());
+// => undefined
+common.debug("typeof sandbox.w1() == undefined");
+assert.equal(typeof sandbox.w1(), "undefined");
 
 // Fails
-// TypeError: Converting circular structure to JSON
-common.debug("sandbox.w() == sandbox.window");
-assert.equal(sandbox.w(), sandbox.window);
+common.debug("sandbox.w1() == sandbox.box.contents");
+assert.equal(sandbox.w1(), sandbox.box.contents);
+
+// Passes
+common.debug("sandbox.w1() == sandbox");
+assert.notEqual(sandbox.w1(), sandbox);
+
+// Passes
+common.debug("sandbox.w1() == sandbox.window");
+assert.notEqual(sandbox.w1(), sandbox.window);
 
 // Passes
 common.debug("sandbox.x == 'x'");
@@ -44,6 +66,10 @@ assert.equal(sandbox.x, "x");
 // Passes
 common.debug("sandbox.y() == 'x'");
 assert.equal(sandbox.y(), "x");
+
+// Passes
+common.debug("sandbox.z1() == 'x'");
+assert.equal(sandbox.z1(), "x");
 
 // Fails
 common.debug("sandbox.z() == 'x'");
